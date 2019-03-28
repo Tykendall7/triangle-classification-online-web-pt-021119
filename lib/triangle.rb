383c5cdde146cf983ddcc8431ab5bf7557940a6b
Tymeckia Kendall <tykend@gmail.com>
2019-03-28 22:28:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,11 @@
 class Triangle
   # write code here
-  attr_accessor equilateral, isosceles, 
+  attr_accessor :equilateral, :isosceles, :scalene
   
+  def initalize (equilateral, isosceles, scalene)
+    @equilateral= equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
   
 end
